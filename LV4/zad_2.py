facc69fa290ad64fc08fda9c3ff8e07d5506d449
sarasajkovski <sara.sajkovski@gmail.com>
2025-04-08 23:07:18 +0200

    Create zad_2.py

---
+++ LV4/zad_2.py
@@ -0,0 +1,3 @@
+U primjeru 4.1, model ne može uhvatiti oscilacije stvarne funkcije (jer koristi samo x).
+
+U primjeru 4.2, dodavanjem viših potencija x, model bolje aproksimira nelinearnu funkciju, što dovodi do manje pogreške, ali može doći i do preprilagodbe.
