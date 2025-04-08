9c8627ce95d772a0636c44af87b85b8d7c57fe6e
sarasajkovski <sara.sajkovski@gmail.com>
2025-04-08 19:11:07 +0200

    Delete zad1.py

--- zad1.py
+++
@@ -1,9 +0,0 @@
-def total_euro(sati, iznos):
-    return sati * iznos
-
-sati = float(input("Radni sati: "))
-iznos = float(input("Eura/h: "))
-ukupno = total_euro(sati, iznos)
-
-print(f"Ukupno: {ukupno} eura")
-
