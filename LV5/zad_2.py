1dda1830aa3a8c9b294a6711228940f21628d870
sarasajkovski <sara.sajkovski@gmail.com>
2025-04-09 00:05:20 +0200

    Update zad_2.py

--- LV5/zad_2.py
+++ LV5/zad_2.py
@@ -1,37 +1,54 @@
-# a)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-X = df[['S3_Temp', 'S5_CO2']].to_numpy()
-y = df['Room_Occupancy_Count'].to_numpy()
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, stratify=y, random_state=42)
-
-# b)
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+
+df = pd.read_csv('C:\\Users\\student\\Desktop\\lV5\\occupancy_processed.csv')
+feature_names = ['S3_Temp', 'S5_CO2']
+target_name = 'Room_Occupancy_Count'
+class_names = ['Slobodna', 'Zauzeta']
+X = df[feature_names].to_numpy()
+y = df[target_name].to_numpy()
+
+plt.figure()
+for class_value in np.unique(y):
+    mask = y == class_value
+    plt.scatter(X[mask, 0], X[mask, 1], label=class_names[class_value])
+plt.xlabel('S3_Temp')
+plt.ylabel('S5_CO2')
+plt.title('Zauzetost prostorije')
+plt.legend()
+plt.show()
+
+# a) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+# b) 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# c)
-from sklearn.neighbors import KNeighborsClassifier
+# c) 
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train_scaled, y_train)
 
-# d)
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+# d) 
 y_pred = knn.predict(X_test_scaled)
-# Matrica zabune
+# a.
 conf_matrix = confusion_matrix(y_test, y_pred)
 print("Matrica zabune:\n", conf_matrix)
-# Točnost klasifikacije
+# b. 
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Točnost: {accuracy:.4f}")
-# Preciznost i odziv po klasama
+print(f"Točnost klasifikacije: {accuracy:.4f}")
+# c. 
 precision = precision_score(y_test, y_pred, average=None)
 recall = recall_score(y_test, y_pred, average=None)
-print("Preciznost po klasama:", precision)
-print("Odziv po klasama:", recall)
-# TOČNOST: 95.56%
+print(f"Preciznost po klasama: {precision}")
+print(f"Odziv po klasama: {recall}")
+
 
 #e) Što se događa s rezultatima ako se koristi veći odnosno manji broj susjeda?
 # Ako se koristi veći broj susjeda model će biti stabilniji, dok je s manjim brojem susjeda veća preciznost
